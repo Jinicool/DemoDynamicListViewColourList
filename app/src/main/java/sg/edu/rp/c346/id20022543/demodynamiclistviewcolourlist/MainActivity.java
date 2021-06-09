41b114e124b21d08b80f6268f73483e2b0903d6a
20022543 <20022543@myrp.edu.sg>
2021-06-09 22:15:49 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20022543/demodynamiclistviewcolourlist/MainActivity.java
@@ -0,0 +1,87 @@
+package sg.edu.rp.c346.id20022543.demodynamiclistviewcolourlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etElement, etIndexElement;
+    Button btnAdd,btnRemove,btnUpdate;
+    ListView lvColour;
+    ArrayList<String> alColours;
+    ArrayAdapter<String> aaColour; //declares as an array adapter of string
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etElement = findViewById(R.id.editTextColor);
+        btnAdd = findViewById(R.id.buttonAddItem);
+        lvColour = findViewById(R.id.listViewColour);
+        etIndexElement = findViewById(R.id.editTextIndex);
+        btnRemove = findViewById(R.id.buttonRemoveItem);
+        btnUpdate = findViewById(R.id.buttonUpdateItem);
+
+        alColours = new ArrayList<>();
+        alColours.add("Red");
+        alColours.add("Orange");
+
+        aaColour = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,alColours); //traffic controller link to the data
+        lvColour.setAdapter(aaColour); //UI connected to traffic controller
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newColour = etElement.getText().toString();
+                int pos = Integer.parseInt(etIndexElement.getText().toString());
+                alColours.add(pos,newColour); //everytime we adding to the arraylist need to notify the traffic controller
+                aaColour.notifyDataSetChanged();
+                etElement.setText("");
+            }
+        });
+
+        lvColour.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,alColours.get(position),Toast.LENGTH_LONG).show();
+                etIndexElement.setText(""+position);
+            }
+        });
+
+        btnRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int pos = Integer.parseInt(etIndexElement.getText().toString());
+                alColours.remove(pos);
+                aaColour.notifyDataSetChanged();
+                etIndexElement.setText(null);
+
+            }
+        });
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String updateColour = etElement.getText().toString();
+                int pos = Integer.parseInt(etIndexElement.getText().toString());
+                alColours.set(pos,updateColour);
+                aaColour.notifyDataSetChanged();
+                etElement.setText("");
+                etIndexElement.setText("");
+            }
+        });
+
+
+
+    }
+}
